75b2d16bad304d3f814e12a4e0bbd4e9574016eb
T-Sumida <ui.suto05@gmail.com>
2020-08-30 15:30:30 +0900

    [update] finish section7

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+// pub mod a {
+//     pub mod series {
+//         pub mod of {
+//             pub fn nested_modules() {}
+//         }
+//     }
+// }
+
+// use a::series::of;
+// fn main() {
+//     // a::series::of::nested_modules();
+//     of::nested_modules();
+// }
+
+enum TrafficLight {
+    Red,
+    Yellow,
+    Green,
+}
+
+use TrafficLight::{Red, Yellow};
+
 fn main() {
-    println!("Hello, world!");
+    let red = Red;
+    let yellow = Yellow;
+    let green = TrafficLight::Green;
 }
