5f0e5acfd817828c5a8cc49c20a909c0891ba754
T-Sumida <ui.suto05@gmail.com>
2020-09-04 17:29:33 +0900

    [update] finish section12

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+extern crate rust_learning;
+
+use std::env;
+use std::process;
+
+use rust_learning::Config;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        // 引数解析時に問題
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = rust_learning::run(config) {
+        eprintln!("Application error: {}", e);
+
+        process::exit(1);
+    }
 }
