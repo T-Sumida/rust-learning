d5cd7df03b21edf08a2893148f3fd43acdf3cd2a
T-Sumida <ui.suto05@gmail.com>
2020-09-05 08:45:59 +0900

    [update] finish section15

---
+++ src/lib.rs
@@ -0,0 +1,82 @@
+// pub trait Messenger {
+//     fn send(&self, msg: &str);
+// }
+
+// pub struct LimitTracker<'a, T: 'a + Messenger> {
+//     messenger: &'a T,
+//     value: usize,
+//     max: usize,
+// }
+
+// impl<'a, T> LimitTracker<'a, T>
+// where
+//     T: Messenger,
+// {
+//     pub fn new(messenger: &T, max: usize) -> LimitTracker<T> {
+//         LimitTracker {
+//             messenger,
+//             value: 0,
+//             max,
+//         }
+//     }
+
+//     pub fn set_value(&mut self, value: usize) {
+//         self.value = value;
+
+//         let percentage_of_max = self.value as f64 / self.max as f64;
+
+//         if percentage_of_max >= 0.75 && percentage_of_max < 0.9 {
+//             // 警告: 割り当ての75％以上を使用してしまいました
+//             self.messenger
+//                 .send("Warning: You've used up over 75% of your quota!");
+//         } else if percentage_of_max >= 0.9 && percentage_of_max < 1.0 {
+//             // 切迫した警告: 割り当ての90%以上を使用してしまいました
+//             self.messenger
+//                 .send("Urgent warning: You've used up over 90% of your quota!");
+//         } else if percentage_of_max >= 1.0 {
+//             // エラー: 割り当てを超えています
+//             self.messenger.send("Error: You are over your quota!");
+//         }
+//     }
+// }
+
+// impl Messenger for MockMessenger {
+//     fn send(&self, message: &str) {
+//         let mut one_borrow = self.sent_messages.borrow_mut();
+//         let mut two_borrow = self.sent_messages.borrow_mut();
+
+//         one_borrow.push(String::from(message));
+//         two_borrow.push(String::from(message));
+//     }
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//     use std::cell::RefCell;
+
+//     struct MockMessenger {
+//         sent_messages: RefCell<Vec<String>>,
+//     }
+
+//     impl MockMessenger {
+//         fn new() -> MockMessenger {
+//             MockMessenger {
+//                 sent_messages: RefCell::new(vec![]),
+//             }
+//         }
+//     }
+
+//     impl Messenger for MockMessenger {
+//         fn send(&self, message: &str) {
+//             self.sent_messages.borrow_mut().push(String::from(message));
+//         }
+//     }
+
+//     #[test]
+//     fn it_sends_an_over_75_percent_warning_message() {
+//         // --snip--
+
+//         assert_eq!(mock_messenger.sent_messages.borrow().len(), 1);
+//     }
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,166 @@
+#[derive(Debug)]
+enum List {
+    Cons(i32, RefCell<Rc<List>>),
+    Nil,
+}
+
+impl List {
+    fn tail(&self) -> Option<&RefCell<Rc<List>>> {
+        match *self {
+            Cons(_, ref item) => Some(item),
+            Nil => None,
+        }
+    }
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+use std::cell::RefCell;
+use std::rc::Rc;
+use List::{Cons, Nil};
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+use std::ops::Deref;
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.0
+    }
+}
+
+fn hello(name: &str) {
+    println!("Hello, {}!", name);
+}
+
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        // CustomSmartPointerをデータ`{}`とともにドロップするよ
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    {
+        let b = Box::new(5);
+        println!("b = {}", b);
+    }
+    // {
+    //     let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    // }
+    {
+        let x = 5;
+        let y = &x;
+
+        assert_eq!(5, x);
+        assert_eq!(5, *y);
+    }
+    {
+        let x = 5;
+        let y = Box::new(x);
+
+        assert_eq!(5, x);
+        assert_eq!(5, *y);
+    }
+    {
+        let x = 5;
+        let y = MyBox::new(x);
+
+        assert_eq!(5, x);
+        assert_eq!(5, *y);
+    }
+    {
+        let m = MyBox::new(String::from("Rust"));
+        hello(&m);
+    }
+    {
+        let m = MyBox::new(String::from("Rust"));
+        hello(&(*m)[..]);
+    }
+    {
+        let c = CustomSmartPointer {
+            data: String::from("my stuff"),
+        }; // 俺のもの
+        let d = CustomSmartPointer {
+            data: String::from("other stuff"),
+        }; // 別のもの
+        println!("CustomSmartPointers created.");
+    }
+    {
+        let c = CustomSmartPointer {
+            data: String::from("some data"),
+        };
+        println!("CustomSmartPointer created.");
+        drop(c);
+        // CustomSmartPointerはmainが終わる前にドロップされた
+        println!("CustomSmartPointer dropped before the end of main.");
+    }
+    // {
+    //     let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    //     let b = Cons(3, Rc::clone(&a));
+    //     let c = Cons(4, Rc::clone(&a));
+    // }
+    // {
+    //     let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    //     // a生成後のカウント = {}
+    //     println!("count after creating a = {}", Rc::strong_count(&a));
+    //     let b = Cons(3, Rc::clone(&a));
+    //     // b生成後のカウント = {}
+    //     println!("count after creating b = {}", Rc::strong_count(&a));
+    //     {
+    //         let c = Cons(4, Rc::clone(&a));
+    //         // c生成後のカウント = {}
+    //         println!("count after creating c = {}", Rc::strong_count(&a));
+    //     }
+    //     // cがスコープを抜けた後のカウント = {}
+    //     println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+    // }
+    {
+        let a = Rc::new(Cons(5, RefCell::new(Rc::new(Nil))));
+
+        // aの最初の参照カウント = {}
+        println!("a initial rc count = {}", Rc::strong_count(&a));
+        // aの次の要素は = {:?}
+        println!("a next item = {:?}", a.tail());
+
+        let b = Rc::new(Cons(10, RefCell::new(Rc::clone(&a))));
+
+        // b作成後のaの参照カウント = {}
+        println!("a rc count after b creation = {}", Rc::strong_count(&a));
+        // bの最初の参照カウント = {}
+        println!("b initial rc count = {}", Rc::strong_count(&b));
+        // bの次の要素 = {:?}
+        println!("b next item = {:?}", b.tail());
+
+        if let Some(link) = a.tail() {
+            *link.borrow_mut() = Rc::clone(&b);
+        }
+
+        // aを変更後のbの参照カウント = {}
+        println!("b rc count after changing a = {}", Rc::strong_count(&b));
+        // aを変更後のaの参照カウント = {}
+        println!("a rc count after changing a = {}", Rc::strong_count(&a));
+
+        // Uncomment the next line to see that we have a cycle;
+        // it will overflow the stack
+        // 次の行のコメントを外して循環していると確認してください; スタックオーバーフローします
+        // println!("a next item = {:?}", a.tail());        // aの次の要素 = {:?}
+    }
 }
