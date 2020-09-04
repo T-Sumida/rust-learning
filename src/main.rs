64a13af4a0a59a3865c3beab76fdc7fcd60f36ec
T-Sumida <ui.suto05@gmail.com>
2020-09-04 15:36:56 +0900

    [update] finish section10

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,227 @@
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
+
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+extern crate rust_learning;
+
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
+
+impl<T, U> Point<T, U> {
+    fn mixup<V, W>(self, other: Point<V, W>) -> Point<T, W> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
+fn largest_i32(list: &[i32]) -> i32 {
+    let mut largest = list[0];
+
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+fn largest_char(list: &[char]) -> char {
+    let mut largest = list[0];
+
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+enum Option_i32 {
+    Some(i32),
+    None,
+}
+
+enum Option_f64 {
+    Some(f64),
+    None,
+}
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    {
+        let number_list = vec![34, 50, 25, 100, 65];
+
+        let mut largest = number_list[0];
+
+        for number in number_list {
+            if number > largest {
+                largest = number;
+            }
+        }
+
+        // 最大値は{}です
+        println!("The largest number is {}", largest);
+
+        let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+
+        let mut largest = number_list[0];
+
+        for number in number_list {
+            if number > largest {
+                largest = number;
+            }
+        }
+
+        println!("The largest number is {}", largest);
+    }
+    {
+        let number_list = vec![34, 50, 25, 100, 65];
+
+        let result = largest_i32(&number_list);
+        println!("The largest number is {}", result);
+
+        let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+
+        let result = largest_i32(&number_list);
+        println!("The largest number is {}", result);
+    }
+    {
+        let number_list = vec![34, 50, 25, 100, 65];
+
+        let result = largest_i32(&number_list);
+        println!("The largest number is {}", result);
+
+        let char_list = vec!['y', 'm', 'a', 'q'];
+
+        let result = largest_char(&char_list);
+        println!("The largest char is {}", result);
+    }
+
+    {
+        let number_list = vec![34, 50, 25, 100, 65];
+
+        let result = largest_i32(&number_list);
+        println!("The largest number is {}", result);
+
+        let char_list = vec!['y', 'm', 'a', 'q'];
+
+        let result = largest_char(&char_list);
+        println!("The largest char is {}", result);
+    }
+
+    {
+        let integer = Point { x: 5, y: 10 };
+        let float = Point { x: 1.0, y: 4.0 };
+        // let wont_work = Point { x: 5, y: 4.0 };
+
+        let both_integer = Point { x: 5, y: 10 };
+        let both_float = Point { x: 1.0, y: 4.0 };
+        let integer_and_float = Point { x: 5, y: 4.0 };
+    }
+    {
+        let p1 = Point { x: 5, y: 10.4 };
+        let p2 = Point { x: "Hello", y: 'c' };
+
+        let p3 = p1.mixup(p2);
+
+        println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+    }
+    {
+        let integer = Option_i32::Some(5);
+        let float = Option_f64::Some(5.0);
+    }
+    {
+        use rust_learning::Summary;
+        let tweet = rust_learning::Tweet {
+            username: String::from("horse_ebooks"),
+            // もちろん、ご存知かもしれないようにね、みなさん
+            content: String::from("of course, as you probably already know, people"),
+            reply: false,
+            retweet: false,
+        };
+
+        println!("1 new tweet: {}", tweet.summarize());
+    }
+    {
+        use rust_learning::Summary;
+        let article = rust_learning::NewsArticle {
+            // ペンギンチームがスタンレーカップチャンピオンシップを勝ち取る！
+            headline: String::from("Penguins win the Stanley Cup Championship!"),
+            // ピッツバーグ、ペンシルベニア州、アメリカ
+            location: String::from("Pittsburgh, PA, USA"),
+            // アイスバーグ
+            author: String::from("Iceburgh"),
+            // ピッツバーグ・ペンギンが再度NHL(National Hockey League)で最強のホッケーチームになった
+            content: String::from(
+                "The Pittsburgh Penguins once again are the best
+            hockey team in the NHL.",
+            ),
+        };
+
+        // 新しい記事が利用可能です！ {}
+        println!("New article available! {}", article.summarize());
+    }
+    {
+        use rust_learning::Summary;
+        let tweet = rust_learning::Tweet {
+            username: String::from("horse_ebooks"),
+            content: String::from("of course, as you probably already know, people"),
+            reply: false,
+            retweet: false,
+        };
+
+        println!("1 new tweet: {}", tweet.summarize());
+    }
+    {
+        let number_list = vec![34, 50, 25, 100, 65];
+
+        let result = largest(&number_list);
+        println!("The largest number is {}", result);
+
+        let char_list = vec!['y', 'm', 'a', 'q'];
+
+        let result = largest(&char_list);
+        println!("The largest char is {}", result);
+    }
+
+    {
+        let x = 5; // ----------+-- 'b
+                   //           |
+        let r = &x; // --+-- 'a  |
+                    //   |       |
+        println!("r: {}", r); //   |       |
+                              // --+       |
+    }
+
+    {
+        // 長い文字列は長い
+        let string1 = String::from("long string is long");
+
+        {
+            let string2 = String::from("xyz");
+            let result = longest(string1.as_str(), string2.as_str());
+            println!("The longest string is {}", result);
+        }
+    }
 }
