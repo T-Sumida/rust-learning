e7271c9398d6c5c679100c1337e4164d04bda98e
T-Sumida <ui.suto05@gmail.com>
2020-09-05 09:19:27 +0900

    [update] finish section17

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,60 @@
+extern crate rust_learning;
+use rust_learning::Draw;
+
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        // code to actually draw a select box
+        //セレクトボックスを実際に描画するコード
+    }
+}
+
+use rust_learning::{Button, Screen};
+
 fn main() {
-    println!("Hello, world!");
+    // {
+    //     let screen = Screen {
+    //         components: vec![
+    //             Box::new(SelectBox {
+    //                 width: 75,
+    //                 height: 10,
+    //                 options: vec![
+    //                     // はい
+    //                     String::from("Yes"),
+    //                     // 多分
+    //                     String::from("Maybe"),
+    //                     // いいえ
+    //                     String::from("No"),
+    //                 ],
+    //             }),
+    //             Box::new(Button {
+    //                 width: 50,
+    //                 height: 10,
+    //                 // 了解
+    //                 label: String::from("OK"),
+    //             }),
+    //         ],
+    //     };
+
+    //     screen.run();
+    // }
+
+    {
+        let mut post = Post::new();
+
+        // 今日はお昼にサラダを食べた
+        post.add_text("I ate a salad for lunch today");
+        assert_eq!("", post.content());
+
+        post.request_review();
+        assert_eq!("", post.content());
+
+        post.approve();
+        assert_eq!("I ate a salad for lunch today", post.content());
+    }
 }
